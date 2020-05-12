c0cbe9ca55c1f996aed1ce690cc4f3be803dda95
Bharathi kannan N <59825547+bharathikannan1311@users.noreply.github.com>
2020-05-12 12:36:28 +0530

    Add files via upload

--- Trees/BinarySearchTree/BST.java
+++ Trees/BinarySearchTree/BST.java
@@ -61,20 +61,4 @@ class BST {
         }
     }
 }
-public class Main {
 
-    public static void main(String[] args) {
-        BST bst=new BST();
-        bst.insert(5);
-        bst.insert(4);
-        bst.insert(7);
-        bst.insert(2);
-        bst.insert(6);
-        System.out.println("Inorder");
-        bst.inorder();
-        System.out.println("\nPreorder");
-        bst.preorder();
-        System.out.println("\nPostorder");
-        bst.postorder();
-    }
-}
